7bacf086cbdeb3d4a3feee199b416daf1e5b84bd
Sarah Moss <sarah-moss@live.com>
2022-02-18 04:33:11 +0000

    added edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,11 +3,28 @@ const request = require('request');
 let breedRequest = process.argv
 breedRequest = breedRequest.slice(2);
 
+
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedRequest}`, function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
+  if (error !== null) {
+    console.log('Oopsy daisy, an error has occurred')
+  }
+
+
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   const data = JSON.parse(body);
+  console.log(data);
+  
+  if (data.length === 0) {
+    console.log('Oops, no data found')
+    return 
+  }
+
   console.log(data[0].description);
+
+ 
+  
 
   // console.log(typeof body); // Print the HTML for the Google homepage.
 });
